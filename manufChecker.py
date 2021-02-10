a5e127af97f3a074008a56be03549f72fd613a87
zl6977 <zl-007@126.com>
2021-02-10 19:42:42 +0100

    add setParamRange
    add setParamRange
    add input value check

--- manufChecker.py
+++ manufChecker.py
@@ -29,30 +29,66 @@ class RuleChecker():
         
         result = ""
         if not leg_length_flag:
-            result += "leg_length not OK\n"
+            result += "leg_length not OK<br>"
         if not back_height_flag:
-            result += "back_height_ not OK\n"
+            result += "back_height not OK<br>"
         if not seat_length_flag:
-            result += "seat_length not OK\n"
+            result += "seat_length not OK<br>"
         if not seat_width_flag:
-            result += "seat_width not OK\n"
+            result += "seat_width not OK<br>"
         if not back_tilt_angle_flag:
-            result += "back_tilt_angle not OK\n"
+            result += "back_tilt_angle not OK<br>"
         if not top_rail_added_length_flag:
-            result += "top_rail_added_length not OK\n"
+            result += "top_rail_added_length not OK<br>"
         if result == "":
             result="ALL OK"
         return result
         
     def compare_para_in_range(self, range, valueToCheck):
-        lower=range[0]
-        upper=range[1]
-        flag = True if (lower <= valueToCheck and upper >= valueToCheck) else False
+        if valueToCheck.isnumeric():
+            lower=range[0]
+            upper=range[1]
+            flag = True if (lower <= float(valueToCheck) and upper >= float(valueToCheck)) else False
+        else:
+            flag = False
         return flag
+        
+    def range_to_string(self, range):
+        lower=float(range[0])
+        upper=float(range[1])
+        stringToReutrn = str(lower) +","+ str(upper)
+        return stringToReutrn
+        
+    def string_to_range(self, string):
+        print(string,"\n")
+        pair = string.split("%2C")
+        print(pair)
+        lower=float(pair[0])
+        upper=float(pair[1])
+        rangeToReturn = [lower,upper]
+        return rangeToReturn
 
 # Handler of HTTP requests / responses
 class MyHandler(BaseHTTPRequestHandler):
     rule_checker = RuleChecker()
+    Html_setParamsIntervals="""
+<html><body><h2>set parameters range</h2>
+<form action="/setParamsIntervals" method="post">
+<label for="leg_length_range">Set leg_length_range:</label><br>
+<input type="text" id="leg_length_range" name="leg_length_range" value="<leg_length_range>"><br><br>
+<label for="back_height_range">Set back_height_range:</label><br>
+<input type="text" id="back_height_range" name="back_height_range" value="<back_height_range>"><br><br>
+<label for="seat_length_range">Set seat_length_range:</label><br>
+<input type="text" id="seat_length_range" name="seat_length_range" value="<seat_length_range>"><br><br>
+<label for="seat_width_range">Set seat_width_range:</label><br>
+<input type="text" id="seat_width_range" name="seat_width_range" value="<seat_width_range>"><br><br>
+<label for="back_tilt_angle_range">Set back_tilt_angle_range:</label><br>
+<input type="text" id="back_tilt_angle_range" name="back_tilt_angle_range" value="<back_tilt_angle_range>"><br><br>
+<label for="top_rail_added_length_range">Set top_rail_added_length_range:</label><br>
+<input type="text" id="top_rail_added_length_range" name="top_rail_added_length_range" value="<top_rail_added_length_range>"><br><br>
+<input type="submit" value="Submit">
+</form></body></html>
+"""
     def do_HEAD(s):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
@@ -67,29 +103,14 @@ class MyHandler(BaseHTTPRequestHandler):
         path = s.path
         if path.find("/setParamsIntervals") != -1:
             #TODO - change HTML accordingly
-            s.wfile.write(bytes('<html><body><h2>Chair</h2>', 'utf-8'))
-            s.wfile.write(bytes('<form action="/orderChair" method="post">', 'utf-8'))
-            
-            s.wfile.write(bytes('<label for="sidePairUp">Set Side of the chair (outer)_max:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="sidePairUp" name="sidePairUp" value="1500"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="depthPairUp">Set Depth for the seat_max:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="depthPairUp" name="depthPairUp" value="1100"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="heightPairUp">Set Height for the seat_max:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="heightPairUp" name="heightPairUp" value="1100"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="widthPairUp">Set Width of the seat_max:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="widthPairUp" name="widthPairUp" value="1100"><br><br>', 'utf-8'))
-            
-            s.wfile.write(bytes('<label for="sidePairLow">Set Side of the chair (outer)_min:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="sidePairLow" name="sidePairLow" value="1500"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="depthPairLow">Set Depth for the seat_min:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="depthPairLow" name="depthPairLow" value="1100"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="heightPairLow">Set Height for the seat_min:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="heightPairLow" name="heightPairLow" value="1100"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="widthPairLow">Set Width of the seat_min:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="widthPairLow" name="widthPairLow" value="1100"><br><br>', 'utf-8'))
-            
-            s.wfile.write(bytes('<input type="submit" value="Submit">', 'utf-8'))
-            s.wfile.write(bytes('</form></body></html>', 'utf-8'))
+            htmlToSend = s.Html_setParamsIntervals
+            htmlToSend = htmlToSend.replace("<leg_length_range>",s.rule_checker.range_to_string(s.rule_checker.leg_length_range))
+            htmlToSend = htmlToSend.replace("<back_height_range>",s.rule_checker.range_to_string(s.rule_checker.back_height_range))
+            htmlToSend = htmlToSend.replace("<seat_length_range>",s.rule_checker.range_to_string(s.rule_checker.seat_length_range))
+            htmlToSend = htmlToSend.replace("<seat_width_range>",s.rule_checker.range_to_string(s.rule_checker.seat_width_range))
+            htmlToSend = htmlToSend.replace("<back_tilt_angle_range>",s.rule_checker.range_to_string(s.rule_checker.back_tilt_angle_range))
+            htmlToSend = htmlToSend.replace("<top_rail_added_length_range>",s.rule_checker.range_to_string(s.rule_checker.top_rail_added_length_range))
+            s.wfile.write(bytes(htmlToSend, 'utf-8'))
         else:
             s.wfile.write(bytes('<html><head><title>Cool interface.</title></head>', 'utf-8'))
             s.wfile.write(bytes("<body><p>The path: " + path + "</p>", "utf-8"))
@@ -113,43 +134,21 @@ class MyHandler(BaseHTTPRequestHandler):
             
             #Get the param value
             pairs = param_line.split("&")
-            sidePairUp = pairs[0].split("=")
-            depthPairUp = pairs[1].split("=")
-            heightPairUp = pairs[2].split("=")
-            widthPairUp = pairs[3].split("=")
-            sidePairLow = pairs[4].split("=")
-            depthPairLow = pairs[5].split("=")
-            heightPairLow = pairs[6].split("=")
-            widthPairLow = pairs[7].split("=")
+            s.rule_checker.leg_length_range = s.rule_checker.string_to_range(pairs[0].split("=")[1])
+            s.rule_checker.back_height_range = s.rule_checker.string_to_range(pairs[1].split("=")[1])
+            s.rule_checker.seat_length_range = s.rule_checker.string_to_range(pairs[2].split("=")[1])
+            s.rule_checker.seat_width_range = s.rule_checker.string_to_range(pairs[3].split("=")[1])
+            s.rule_checker.back_tilt_angle_range = s.rule_checker.string_to_range(pairs[4].split("=")[1])
+            s.rule_checker.top_rail_added_length_range = s.rule_checker.string_to_range(pairs[5].split("=")[1])
             
-            s.wfile.write(bytes('<html><body><h2>Chair</h2>', 'utf-8'))
-            s.wfile.write(bytes('<form action="/orderChair" method="post">', 'utf-8'))
-            
-            s.wfile.write(bytes('<label for="sidePairUp">Set Side of the chair (outer)_max:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="sidePairUp" name="sidePairUp" value="1500"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="depthPairUp">Set Depth for the seat_max:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="depthPairUp" name="depthPairUp" value="1100"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="heightPairUp">Set Height for the seat_max:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="heightPairUp" name="heightPairUp" value="1100"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="widthPairUp">Set Width of the seat_max:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="widthPairUp" name="widthPairUp" value="1100"><br><br>', 'utf-8'))
-            
-            s.wfile.write(bytes('<label for="sidePairLow">Set Side of the chair (outer)_min:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="sidePairLow" name="sidePairLow" value="1500"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="depthPairLow">Set Depth for the seat_min:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="depthPairLow" name="depthPairLow" value="1100"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="heightPairLow">Set Height for the seat_min:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="heightPairLow" name="heightPairLow" value="1100"><br><br>', 'utf-8'))
-            s.wfile.write(bytes('<label for="widthPairLow">Set Width of the seat_min:</label><br>', 'utf-8'))
-            s.wfile.write(bytes('<input type="text" id="widthPairLow" name="widthPairLow" value="1100"><br><br>', 'utf-8'))
-            
-            s.wfile.write(bytes('<input type="submit" value="Submit">', 'utf-8'))
-            s.wfile.write(bytes('</form></body></html>', 'utf-8'))
-            
-            s.wfile.write(bytes('<p>The max values was set to ' + str(sidePairUp[1]) + str(depthPairUp[1]) + str(heightPairUp[1]) + str(widthPairUp[1])  + '</p>', 'utf-8'))
-            s.wfile.write(bytes('<p>The min values was set to ' + str(sidePairLow[1]) + str(depthPairLow[1]) + str(heightPairLow[1]) + str(widthPairLow[1])  + '</p>', 'utf-8'))
-            
-            s.wfile.write(bytes('</form></body></html>', 'utf-8'))
+            htmlToSend = s.Html_setParamsIntervals
+            htmlToSend = htmlToSend.replace("<leg_length_range>",s.rule_checker.range_to_string(s.rule_checker.leg_length_range))
+            htmlToSend = htmlToSend.replace("<back_height_range>",s.rule_checker.range_to_string(s.rule_checker.back_height_range))
+            htmlToSend = htmlToSend.replace("<seat_length_range>",s.rule_checker.range_to_string(s.rule_checker.seat_length_range))
+            htmlToSend = htmlToSend.replace("<seat_width_range>",s.rule_checker.range_to_string(s.rule_checker.seat_width_range))
+            htmlToSend = htmlToSend.replace("<back_tilt_angle_range>",s.rule_checker.range_to_string(s.rule_checker.back_tilt_angle_range))
+            htmlToSend = htmlToSend.replace("<top_rail_added_length_range>",s.rule_checker.range_to_string(s.rule_checker.top_rail_added_length_range))
+            s.wfile.write(bytes(htmlToSend, 'utf-8'))
             
         elif path.find("/manufCheck") != -1:
 
@@ -167,7 +166,8 @@ class MyHandler(BaseHTTPRequestHandler):
             back_tilt_angle = pairs[4].split("=")
             top_rail_added_length = pairs[5].split("=")
             
-            parameterSet = [int(leg_length[1]), int(back_height[1]), int(seat_length[1]), int(seat_width[1]), int(back_tilt_angle[1]), int(top_rail_added_length[1])]
+            parameterSet = [leg_length[1], back_height[1], seat_length[1], seat_width[1], back_tilt_angle[1], top_rail_added_length[1]]
+            # parameterSet = [int(leg_length[1]), int(back_height[1]), int(seat_length[1]), int(seat_width[1]), int(back_tilt_angle[1]), int(top_rail_added_length[1])]
             check_result = s.rule_checker.check_manufacutrable(parameterSet)
             
             s.wfile.write(bytes(check_result, 'utf-8'))
--- server_chair_zzz.py
+++ server_chair_zzz.py
@@ -3,7 +3,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os, requests
 
-HOST_NAME = '127.0.0.1' 
+HOST_NAME = '10.24.10.83' 
 PORT_NUMBER = 1234
 
 # Handler of HTTP requests / responses
@@ -86,6 +86,7 @@ class MyHandler(BaseHTTPRequestHandler):
             s.wfile.write(bytes('<label for="top_rail_added_length">top_rail_added_length</label><br>', 'utf-8'))
             s.wfile.write(bytes('<input type="text" id="top_rail_added_length" name="top_rail_added_length" value="10"><br><br>', 'utf-8'))
             
+            s.wfile.write(bytes('<input type="submit" value="Load Default" formaction="/orderChair" formmethod="get">', 'utf-8'))
             s.wfile.write(bytes('<input type="submit" value="Submit">', 'utf-8'))
             s.wfile.write(bytes('</form></body></html>', 'utf-8'))
         else:
@@ -142,8 +143,6 @@ class MyHandler(BaseHTTPRequestHandler):
             s.wfile.write(bytes('<html><body><h2>Chair</h2>', 'utf-8'))
             s.wfile.write(bytes('<form action="/orderChair" method="post">', 'utf-8'))
             
-            s.wfile.write(bytes('<p>The following parameters have arrived: ' + str(leg_length[1]) + ', ' + str(back_height[1]) + ', ' + str(seat_length[1]) + ', ' + str(seat_width[1]) + ', ' +str(back_tilt_angle[1]) +', ' + str(top_rail_added_length[1]) + '</p>', 'utf-8'))
-            
             s.wfile.write(bytes('<label for="leg_length">leg_length:</label><br>', 'utf-8'))
             s.wfile.write(bytes('<input type="text" id="leg_length" name="leg_length" value="' + str(leg_length[1]) + '"><br><br>', 'utf-8'))
             s.wfile.write(bytes('<label for="back_height">back_height</label><br>', 'utf-8'))
@@ -157,17 +156,27 @@ class MyHandler(BaseHTTPRequestHandler):
             s.wfile.write(bytes('<label for="top_rail_added_length">top_rail_added_length</label><br>', 'utf-8'))
             s.wfile.write(bytes('<input type="text" id="top_rail_added_length" name="top_rail_added_length" value="' + str(top_rail_added_length[1]) + '"><br><br>', 'utf-8'))
             
+            s.wfile.write(bytes('<input type="submit" value="Load Default" formaction="/orderChair" formmethod="get">', 'utf-8'))
             s.wfile.write(bytes('<input type="submit" value="Submit">', 'utf-8'))
-            s.wfile.write(bytes('</form></body></html>', 'utf-8'))
             
+            # s.wfile.write(bytes('<p>The following parameters have arrived: ' 
+                                # + str(leg_length[1]) + ', ' + str(back_height[1]) + ', ' 
+                                # + str(seat_length[1]) + ', ' + str(seat_width[1]) + ', ' 
+                                # +str(back_tilt_angle[1]) +', ' + str(top_rail_added_length[1]) + '</p>', 'utf-8'))
+            s.wfile.write(bytes('<p>The following parameters have arrived: ' 
+                                + leg_length[1] + ', ' + back_height[1] + ', ' 
+                                + seat_length[1] + ', ' + seat_width[1] + ', ' 
+                                + back_tilt_angle[1] +', ' + top_rail_added_length[1] + '</p>', 'utf-8'))
             #manufacurable check-query the result from the manufChecker.py server
-            dataToSend = "leg_length="+str(leg_length[1])+"&back_height="+str(back_height[1])+"&seat_length="+str(seat_length[1])
-            dataToSend += "&seat_width="+str(seat_width[1])+"&back_tilt_angle="+str(back_tilt_angle[1])+"&top_rail_added_length="+str(top_rail_added_length[1])
-            
+            # dataToSend = "leg_length="+str(leg_length[1])+"&back_height="+str(back_height[1])+"&seat_length="+str(seat_length[1])
+            # dataToSend += "&seat_width="+str(seat_width[1])+"&back_tilt_angle="+str(back_tilt_angle[1])+"&top_rail_added_length="+str(top_rail_added_length[1])
+            dataToSend = [leg_length,back_height,seat_length,seat_width,back_tilt_angle,top_rail_added_length]
             rulecheckResult= check_manufacutrable("http://127.0.0.1:4321/manufCheck", dataToSend)
-            s.wfile.write(bytes('<p>The rule check result: ' + rulecheckResult + '</p>', 'utf-8'))
-            update_template(leg_length[1],back_height[1],seat_length[1],seat_width[1],back_tilt_angle[1],top_rail_added_length[1])
+            s.wfile.write(bytes('<p style="color:DodgerBlue;">The rule check result: <br>' + rulecheckResult[1] + '</p>', 'utf-8'))
+            if rulecheckResult[0]:
+                update_template(leg_length[1],back_height[1],seat_length[1],seat_width[1],back_tilt_angle[1],top_rail_added_length[1])
             
+            s.wfile.write(bytes('</form></body></html>', 'utf-8'))
 
 def update_template(leg_length,back_height,seat_length,seat_width,back_tilt_angle,top_rail_added_length):
     f = open(os.path.dirname(os.path.abspath(__file__))+"\\templates\\chair_zzz.dfa","r")
@@ -188,7 +197,11 @@ def update_template(leg_length,back_height,seat_length,seat_width,back_tilt_angl
 def check_manufacutrable(url, dataToSend):
     x = requests.post(url, data = dataToSend)
     #print the response text (the content of the requested file):
-    return x.text
+    resultToReturn = [False,""]
+    if x.text == "ALL OK":
+        resultToReturn = [True, x.text]
+    else: resultToReturn = [False, x.text]
+    return resultToReturn
 
 if __name__ == '__main__':
     server_class = HTTPServer
